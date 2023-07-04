8ac44f3de0fb7c91ffbab14ae9dc4d12546a2e2b
Aaron Bojarski <abojarski@ethz.ch>
2023-07-04 09:48:12 +0200

    format

--- ms_app.py
+++ ms_app.py
@@ -5,11 +5,10 @@ from zaber_motion.binary import Connection, DeviceSettings, BinarySettings, Comm
 from mini_stretcher import color_LED, zaber_stuff
 
 
-
 class SetupFrame(ttk.Labelframe):
     def __init__(self, master, motors):
         super().__init__(master, text="Setup", padding=(5, 5))
-        
+
         self.port_var = ttk.StringVar(value="COM3")
         self.port_ent = ttk.Entry(self, textvariable=self.port_var, width=6)
         # self.port_ent.configure(validate="key", validatecommand=(self.register(self.validator), "%P",))
@@ -20,10 +19,10 @@ class SetupFrame(ttk.Labelframe):
 
         self.connect_led = color_LED.ColorLED(self)
         self.connect_led.grid(row=0, column=2, padx=5, pady=2)
-        
+
         self.home_btn = ttk.Button(self, text="Home stages", state="disabled", command=self.on_home_click)
         self.home_btn.grid(row=1, column=1, padx=5, pady=2)
-        
+
         self.home_led = color_LED.ColorLED(self)
         self.home_led.grid(row=1, column=2, padx=5, pady=2)
 
@@ -32,16 +31,15 @@ class SetupFrame(ttk.Labelframe):
         self.home_state = "naive"
         self.device1 = None
         self.device2 = None
-        
-        self.motors = motors
 
+        self.motors = motors
 
     # def validator(self, P):
     #     return P.isdigit() or P == ""
-    
+
     # def validator(self, P):
-        # return P.__len__() or P == ""
-        
+    # return P.__len__() or P == ""
+
     def on_connect_click(self):
         if self.connection_state == "naive":
             try:
@@ -63,7 +61,7 @@ class SetupFrame(ttk.Labelframe):
                 self.connect_btn.configure(bootstyle="defaul", text="Connect")
                 self.connect_led.set_color("red")
                 self.home_led.set_color("red")
-                
+
     def on_home_click(self):
         try:
             self.motors.home()
@@ -79,8 +77,6 @@ class SetupFrame(ttk.Labelframe):
         print(self.port_var.get())
 
 
-
-        
 class ManualMove(ttk.Labelframe):
     def __init__(self, master, motors):
         super().__init__(master, text="Manual move", padding=(5, 5))
@@ -95,25 +91,18 @@ class ManualMove(ttk.Labelframe):
 
         self.length_lbl = ttk.Label(self, text="Length [mm]")
         self.length_lbl.grid(row=0, column=0, sticky=E)
-        self.length_ent = ttk.Entry(self,
-                                    textvariable=self.length_var,
-                                    width=6,
-                                    justify="right")
+        self.length_ent = ttk.Entry(self, textvariable=self.length_var, width=6, justify="right")
         self.length_ent.grid(row=0, column=1, padx=5, pady=2, sticky=EW)
 
         self.speed_lbl = ttk.Label(self, text="Speed [mm/s]")
         self.speed_lbl.grid(row=1, column=0, sticky=E)
-        self.speed_ent = ttk.Entry(self,
-                                   textvariable=self.speed_var,
-                                   width=6,
-                                   justify="right")
+        self.speed_ent = ttk.Entry(self, textvariable=self.speed_var, width=6, justify="right")
         self.speed_ent.grid(row=1, column=1, padx=5, pady=2, sticky=EW)
 
         self.move_btn = ttk.Button(self, text="Move", command=self.on_move_click)
         self.move_btn.grid(row=3, column=1, padx=5, pady=5, sticky=EW)
 
         self.motors = motors
-
 
     def on_move_click(self):
         try:
@@ -127,7 +116,7 @@ class ManualMove(ttk.Labelframe):
             print(f"Could not convert length {self.length_var.get()} into a number.")
             return
 
-        
+
 class ProtocolFrame(ttk.Labelframe):
     def __init__(self, master):
         super().__init__(master, text="Protocol", padding=(5, 5))
@@ -146,34 +135,22 @@ class ProtocolFrame(ttk.Labelframe):
 
         self.len_zero_lab = ttk.Label(self, text="L0 [mm]")
         self.len_zero_lab.grid(row=0, column=0, sticky=E)
-        self.len_zero_ent = ttk.Entry(self,
-                                      textvariable=self.len_zero_var,
-                                      width=6,
-                                      justify="right")
+        self.len_zero_ent = ttk.Entry(self, textvariable=self.len_zero_var, width=6, justify="right")
         self.len_zero_ent.grid(row=0, column=1, padx=5, pady=2, sticky=E)
 
         self.len_target_lab = ttk.Label(self, text="Target length [mm]")
         self.len_target_lab.grid(row=1, column=0, sticky=E)
-        self.len_target_ent = ttk.Entry(self,
-                                        textvariable=self.len_target_var,
-                                        width=6,
-                                        justify="right")
+        self.len_target_ent = ttk.Entry(self, textvariable=self.len_target_var, width=6, justify="right")
         self.len_target_ent.grid(row=1, column=1, padx=5, pady=2, sticky=E)
 
         self.pause_lab = ttk.Label(self, text="Pause [s]")
         self.pause_lab.grid(row=2, column=0, sticky=E)
-        self.pause_ent = ttk.Entry(self,
-                                   textvariable=self.pause_var,
-                                   width=6,
-                                   justify="right")
+        self.pause_ent = ttk.Entry(self, textvariable=self.pause_var, width=6, justify="right")
         self.pause_ent.grid(row=2, column=1, padx=5, pady=2, sticky=E)
 
         self.speed_lab = ttk.Label(self, text="Speed [mm/s]")
         self.speed_lab.grid(row=3, column=0, sticky=E)
-        self.speed_ent = ttk.Entry(self,
-                                   textvariable=self.speed_var,
-                                   width=6,
-                                   justify="right")
+        self.speed_ent = ttk.Entry(self, textvariable=self.speed_var, width=6, justify="right")
         self.speed_ent.grid(row=3, column=1, padx=5, pady=2, sticky=E)
 
 
@@ -183,7 +160,7 @@ class ControlsFrame(ttk.Labelframe):
         # self.pack(fill=BOTH, expand=True, padx=5, pady=2)
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
-        
+
         self.run_btn = ttk.Button(self, text="Run protocol", bootstyle="secondary")
         self.run_btn.grid(row=0, column=0, padx=5, pady=5, sticky=EW)
 
@@ -195,8 +172,8 @@ class ControlsFrame(ttk.Labelframe):
 
         self.stop_btn = ttk.Button(self, text="STOP", bootstyle="danger")
         self.stop_btn.grid(row=1, column=1, padx=5, pady=5, sticky=EW)
-        
-        
+
+
 class StatusFrame(ttk.Labelframe):
     def __init__(self, master):
         super().__init__(master, text="Status", padding=(5, 5))
@@ -208,12 +185,13 @@ class StatusFrame(ttk.Labelframe):
         self.status_lbl.grid(row=0, column=0, padx=5, pady=2, sticky=E)
         self.status_out = ttk.Label(self, text="idle")
         self.status_out.grid(row=0, column=1, padx=5, pady=2, sticky=W)
-        
+
         self.clen_lbl = ttk.Label(self, text="Current length [mm]:")
         self.clen_lbl.grid(row=1, column=0, padx=5, pady=2, sticky=E)
         self.clen_out = ttk.Label(self, text="12")
         self.clen_out.grid(row=1, column=1, padx=5, pady=2, sticky=W)
-        
+
+
 class Motors:
     def __init__(self):
         self.connected = False
@@ -223,7 +201,7 @@ class Motors:
         self.device1 = self.connection.detect_devices()[0]
         self.device2 = self.connection.detect_devices()[1]
         self.connected = True
-    
+
     def disconnect(self):
         self.connected = False
         self.connection.close()
@@ -236,7 +214,6 @@ class Motors:
 
     def move(self):
         pass
-
 
 
 if __name__ == "__main__":
