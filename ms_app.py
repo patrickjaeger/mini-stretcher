e11c961729c78aabfd0ede48ea02da0af44d670e
Aaron Bojarski <abojarski@ethz.ch>
2023-07-04 09:45:37 +0200

    add motors class

--- ms_app.py
+++ ms_app.py
@@ -7,7 +7,7 @@ from mini_stretcher import color_LED, zaber_stuff
 
 
 class SetupFrame(ttk.Labelframe):
-    def __init__(self, master):
+    def __init__(self, master, motors):
         super().__init__(master, text="Setup", padding=(5, 5))
         
         self.port_var = ttk.StringVar(value="COM3")
@@ -21,7 +21,7 @@ class SetupFrame(ttk.Labelframe):
         self.connect_led = color_LED.ColorLED(self)
         self.connect_led.grid(row=0, column=2, padx=5, pady=2)
         
-        self.home_btn = ttk.Button(self, text="Home stages", command=self.on_home_click)
+        self.home_btn = ttk.Button(self, text="Home stages", state="disabled", command=self.on_home_click)
         self.home_btn.grid(row=1, column=1, padx=5, pady=2)
         
         self.home_led = color_LED.ColorLED(self)
@@ -33,6 +33,9 @@ class SetupFrame(ttk.Labelframe):
         self.device1 = None
         self.device2 = None
         
+        self.motors = motors
+
+
     # def validator(self, P):
     #     return P.isdigit() or P == ""
     
@@ -42,38 +45,35 @@ class SetupFrame(ttk.Labelframe):
     def on_connect_click(self):
         if self.connection_state == "naive":
             try:
-                zaber_stuff.connect()
-                self.connection = Connection.open_serial_port(self.port_var.get())
-                self.device1 = self.connection.detect_devices()[0]
-                self.device2 = self.connection.detect_devices()[1]
+                self.motors.connect(self.port_var.get())
             except Exception as e:
                 print(e)
             else:
                 self.connection_state = "connected"
                 self.connect_btn.configure(bootstyle="danger", text="Disconnect")
                 self.connect_led.set_color("green")
+                self.home_btn.configure(state="enabled")
         elif self.connection_state == "connected":
             try:
-                self.connection.close()
+                self.motors.disconnect()
             except:
                 return
             else:
                 self.connection_state = "naive"
                 self.connect_btn.configure(bootstyle="defaul", text="Connect")
                 self.connect_led.set_color("red")
+                self.home_led.set_color("red")
                 
     def on_home_click(self):
-        print(self.port_var.get())
-        # try:
-        #     self.device1.generic_command_no_response(CommandCode.HOME)
-        #     self.device2.generic_command_no_response(CommandCode.HOME)
-        #     self.home_led.set_color("yellow")
-        # except:
-        #     return
-        # else:
-        #     self.home_btn.configure(state="disabled")
-        #     self.home_state = "homed"
-        #     self.home_led.set_color("green")
+        try:
+            self.motors.home()
+            self.home_led.set_color("yellow")
+        except Exception as e:
+            print(e)
+        else:
+            self.home_btn.configure(state="disabled")
+            self.home_state = "homed"
+            self.home_led.set_color("green")
 
     def callback(self):
         print(self.port_var.get())
@@ -82,7 +82,7 @@ class SetupFrame(ttk.Labelframe):
 
         
 class ManualMove(ttk.Labelframe):
-    def __init__(self, master):
+    def __init__(self, master, motors):
         super().__init__(master, text="Manual move", padding=(5, 5))
         # self.pack(fill=BOTH, expand=True, padx=5, pady=2)
         self.columnconfigure(0, weight=1, minsize=120)
@@ -109,8 +109,23 @@ class ManualMove(ttk.Labelframe):
                                    justify="right")
         self.speed_ent.grid(row=1, column=1, padx=5, pady=2, sticky=EW)
 
-        self.move_btn = ttk.Button(self, text="Move")
+        self.move_btn = ttk.Button(self, text="Move", command=self.on_move_click)
         self.move_btn.grid(row=3, column=1, padx=5, pady=5, sticky=EW)
+
+        self.motors = motors
+
+
+    def on_move_click(self):
+        try:
+            speed = float(self.speed_var.get())
+        except:
+            print(f"Could not convert speed {self.speed_var.get()} into a number.")
+            return
+        try:
+            length = float(self.length_var.get())
+        except:
+            print(f"Could not convert length {self.length_var.get()} into a number.")
+            return
 
         
 class ProtocolFrame(ttk.Labelframe):
@@ -199,14 +214,40 @@ class StatusFrame(ttk.Labelframe):
         self.clen_out = ttk.Label(self, text="12")
         self.clen_out.grid(row=1, column=1, padx=5, pady=2, sticky=W)
         
+class Motors:
+    def __init__(self):
+        self.connected = False
+
+    def connect(self, port):
+        self.connection = Connection.open_serial_port(port)
+        self.device1 = self.connection.detect_devices()[0]
+        self.device2 = self.connection.detect_devices()[1]
+        self.connected = True
+    
+    def disconnect(self):
+        self.connected = False
+        self.connection.close()
+
+    def home(self):
+        if not self.connected:
+            raise ConnectionError("Motors must be connected first.")
+        self.device1.generic_command_no_response(CommandCode.HOME)
+        self.device2.generic_command_no_response(CommandCode.HOME)
+
+    def move(self):
+        pass
+
+
 
 if __name__ == "__main__":
     # Library.enable_device_db_store()
 
     app = ttk.Window("miniStretcher", "darkly", resizable=(False, False), iconphoto="icons/banana2.png")
 
-    SetupFrame(app).grid(row=0, column=0, sticky=NSEW, padx=5, pady=2)
-    ManualMove(app).grid(row=0, column=1, sticky=NSEW, padx=5, pady=2)
+    motors = Motors()
+
+    SetupFrame(app, motors).grid(row=0, column=0, sticky=NSEW, padx=5, pady=2)
+    ManualMove(app, motors).grid(row=0, column=1, sticky=NSEW, padx=5, pady=2)
     ProtocolFrame(app).grid(row=1, column=0, rowspan=2, sticky=NSEW, padx=5, pady=2)
     ControlsFrame(app).grid(row=1, column=1, sticky=NSEW, padx=5, pady=2)
     StatusFrame(app).grid(row=2, column=1, sticky=NSEW, padx=5, pady=2)
